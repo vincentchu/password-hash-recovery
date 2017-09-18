ca72200b2f2e66cf6ec8d07299d7c4e36c324ca1
Vincent Chu <vincentchu@gmail.com>
2017-09-18 09:49:11 -0700

    a few tweaks

--- client/src/views/Events.js
+++ client/src/views/Events.js
@@ -11,34 +11,40 @@ import type { Contract } from '../state/contracts'
 
 const EventsTable = (props: {
   title: string,
+  emptyText: string,
   events: Event[],
   coinbase: ?string,
 }) => {
-  const { coinbase, title, events } = props
+  const { coinbase, title, emptyText, events } = props
 
   return (
-    <div className="row">
+    <div className="row events-table">
       <div className="col-md-12">
-        <h4>{title}</h4>
-        <Table condensed hover>
-          <thead>
-            <tr>
-              <th>Transaction</th>
-              <th>Plaintext Password</th>
-            </tr>
-          </thead>
-          <tbody>
-            { reverse(events).map((evt) => (
-              <tr key={evt.transactionHash}>
-                <td>
-                  <HashLink addr={evt.transactionHash} truncate />
-                  { coinbase === (evt.args.source || evt.args.crackedBy || -1) && ' (you)' }
-                </td>
-                <td>{evt.args.password}</td>
+        <h4>{ title }</h4>
+
+        { events.length === 0 && emptyText }
+
+        { events.length > 0 && (
+          <Table condensed hover>
+            <thead>
+              <tr>
+                <th>Transaction</th>
+                <th>Plaintext Password</th>
               </tr>
-            )) }
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              { reverse(events).map((evt) => (
+                <tr key={evt.transactionHash}>
+                  <td>
+                    <HashLink addr={evt.transactionHash} truncate />
+                    { coinbase === (evt.args.source || evt.args.crackedBy || -1) && ' (you)' }
+                  </td>
+                  <td>{evt.args.password}</td>
+                </tr>
+              )) }
+            </tbody>
+          </Table>
+        ) }
       </div>
     </div>
   )
@@ -52,8 +58,15 @@ const Events = (props: {
   const { coinbase, passwordCrackedEvents, attemptFailedEvents } = props
   return (
     <div>
-      <EventsTable title="Successful Cracks" events={passwordCrackedEvents} coinbase={coinbase} />
-      <EventsTable title="Failed Attempts" events={attemptFailedEvents} coinbase={coinbase} />
+      <EventsTable
+        title="Successful Cracks" emptyText="No Successful Cracks"
+        events={passwordCrackedEvents} coinbase={coinbase}
+      />
+
+      <EventsTable
+        title="Failed Attempts" emptyText="No Failed Attempts"
+        events={attemptFailedEvents} coinbase={coinbase}
+      />
     </div>
   )
 }
