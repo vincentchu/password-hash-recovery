24cfd92439f5b59793c5ff9fb20a7a3c85343a25
Vincent Chu <vincentchu@gmail.com>
2017-09-19 12:06:47 -0700

    Add in contracts

--- client/src/state/contracts.js
+++ client/src/state/contracts.js
@@ -20,7 +20,7 @@ const mergeValidity = (contracts: Contract[], address: string, validity: ?bool):
 
   contracts.forEach((contract) => {
     if (contract.contractAddress === address) {
-      contract.validTest = validity
+      contract.validTest = validity || false
     }
 
     updatedContracts.push(contract)
@@ -38,18 +38,20 @@ const initState = {
       passwordSha1Hash: '0xcbfdac6008f9cab4083784cbd1874f76618d2a97',
       validTest: false,
     },
-    // {
-    //   title: 'Medium Difficulty',
-    //   panelStyle: 'warning',
-    //   contractAddress: '0x836ae85ccef18ef6738d9f8fa0f2e118ec804c1c',
-    //   passwordSha1Hash: '0x9373e7c1555c33721d878e445a812cc577719a61',
-    // },
-    // {
-    //   title: 'Challenging Difficulty',
-    //   panelStyle: 'danger',
-    //   contractAddress: '0xce7d7e78270ceaee4c1f93f96802a504a84b6eaa',
-    //   passwordSha1Hash: '0x1cee85b543db7ce8d3536e6a542ddb77d15009d7',
-    // },
+    {
+      title: 'Medium Difficulty',
+      panelStyle: 'warning',
+      contractAddress: '0x836ae85ccef18ef6738d9f8fa0f2e118ec804c1c',
+      passwordSha1Hash: '0x9373e7c1555c33721d878e445a812cc577719a61',
+      validTest: false,
+    },
+    {
+      title: 'Challenging Difficulty',
+      panelStyle: 'danger',
+      contractAddress: '0xce7d7e78270ceaee4c1f93f96802a504a84b6eaa',
+      passwordSha1Hash: '0x1cee85b543db7ce8d3536e6a542ddb77d15009d7',
+      validTest: false,
+    },
   ],
 }
 
