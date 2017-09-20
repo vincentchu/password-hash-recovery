e0aacbda7b0c5aabe3854e6e7ad5a3ebf43b834a
Vincent Chu <vincentchu@gmail.com>
2017-09-20 14:50:59 -0700

    Deploy to rinkeby!

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -10,5 +10,11 @@ module.exports = {
       port: 8545,
       network_id: '*', // Match any network id
     },
+    rinkeby: {
+      host: 'localhost',
+      port: 8545,
+      from: '0x65df7722230418a8cef2954a4f36a5199fcdd338',
+      network_id: '4',
+    },
   },
 }
