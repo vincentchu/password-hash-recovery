63a69e444d75cb87f3c11679b3bc945889ea3c97
Vincent Chu <vincentchu@gmail.com>
2017-09-26 22:03:18 -0700

    instrument test / submit

--- client/src/analytics.js
+++ client/src/analytics.js
@@ -1,47 +1,13 @@
 // @flow
-import type { SessionStore } from './state/session'
-
-type State = {
-  session: SessionStore
-}
-
-const logTransition = (attr: string, beforeSession: SessionStore, afterSession: SessionStore) => {
-  const before = beforeSession[attr]
-  const after = afterSession[attr]
-
-  if (before !== after) {
-    window.ga('send', {
-      hitType: 'event',
-      eventCategory: attr,
-      eventAction: 'transition',
-      eventLabel: String(after),
-    })
-  }
-}
-
-const logEventIfNeeded = (beforeState: State, afterState: State) => {
+export const sendEvent = (action: string, params: Object = {}) => { // eslint-disable-line import/prefer-default-export, max-len
   try {
-    if (typeof window.ga !== 'undefined') {
-      const beforeSession = beforeState.session
-      const afterSession = afterState.session
-
-      logTransition('web3Present', beforeSession, afterSession)
-      logTransition('network', beforeSession, afterSession)
-      logTransition('coinbase', beforeSession, afterSession)
+    if (window.gtag) {
+      window.gtag('event', action, {
+        ...params,
+        ...window.analyticsParams,
+      })
     }
   } catch (err) {
-    console.log('Error on GA', err)
+    console.log('Detected error sendingEvent', err)
   }
 }
-
-const analytics = ({ getState }: { getState: Function }) => (next: any) => (action: any) => {
-  const beforeState = getState()
-  const ret = next(action)
-  const afterState = getState()
-
-  logEventIfNeeded(beforeState, afterState)
-
-  return ret
-}
-
-export default analytics
--- client/src/redux-store.js
+++ client/src/redux-store.js
@@ -2,7 +2,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import createLogger from 'redux-logger'
-import analytics from './analytics'
 import { reducer as sessionReducer } from './state/session'
 import { reducer as contractsReducer } from './state/contracts'
 import { reducer as eventsReducer } from './state/events'
